4d126aba590b08164185be04eedf2e54a180abb0
Lenin Guerrero Ramírez <faulk65@gmail.com>
2026-03-08 00:38:02 -0500

    feat: customer inquiry flow is created

--- src/main/java/com/investment/funds/infrastructure/configuration/WireDependencies.java
+++ src/main/java/com/investment/funds/infrastructure/configuration/WireDependencies.java
@@ -6,13 +6,14 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import com.investment.funds.application.usecase.CancelSubscribeUseCase;
+import com.investment.funds.application.usecase.GetClientUseCase;
 import com.investment.funds.application.usecase.GetTransactionHistoryUseCase;
-import com.investment.funds.application.usecase.HelperUseCase;
 import com.investment.funds.application.usecase.SubscribeUseCase;
 import com.investment.funds.application.usecase.UseCase;
 import com.investment.funds.application.usecase.dto.CancelSubscribeInput;
 import com.investment.funds.application.usecase.dto.GetTransactionHistoryInput;
 import com.investment.funds.application.usecase.dto.SubscribeInput;
+import com.investment.funds.domain.model.Client;
 import com.investment.funds.domain.model.Transaction;
 import com.investment.funds.domain.port.ClientRepository;
 import com.investment.funds.domain.port.FundRepository;
@@ -26,6 +27,11 @@ public class WireDependencies {
     @Bean
     public TransactionService transactionService(TransactionRepository transactionRepository) {
         return new TransactionService(transactionRepository);
+    }
+
+    @Bean
+    public UseCase<String, Client> getClient(ClientRepository clientRepository) {
+        return new GetClientUseCase(clientRepository);
     }
 
     @Bean
@@ -49,10 +55,5 @@ public class WireDependencies {
     public UseCase<GetTransactionHistoryInput, List<Transaction>> getTransactionHistory(
             TransactionService transactionService) {
         return new GetTransactionHistoryUseCase(transactionService);
-    }
-
-    @Bean
-    public HelperUseCase helperUseCase(FundRepository fundRepository, ClientRepository clientRepository) {
-        return new HelperUseCase(fundRepository, clientRepository);
     }
 }
