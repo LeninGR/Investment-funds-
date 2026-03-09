8612c32b1771bb4bc85d185d01f7fb8d06cc9908
Lenin Guerrero Ramírez <faulk65@gmail.com>
2026-03-09 09:48:41 -0500

    fix: restaurar Import DispatcherServlet y agregar EnableWebMvc para forzar carga completa de stack web en Lambda

--- src/main/java/com/investment/funds/InvestmentFundsApplication.java
+++ src/main/java/com/investment/funds/InvestmentFundsApplication.java
@@ -2,11 +2,16 @@ package com.investment.funds;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.web.servlet.DispatcherServletAutoConfiguration;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.Import;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @SpringBootApplication
+@EnableWebMvc
 @EnableMongoRepositories(basePackages = "com.investment.funds.infrastructure.adapter.mongodb.repository")
+@Import(DispatcherServletAutoConfiguration.class)
 public class InvestmentFundsApplication extends SpringBootServletInitializer {
 
 	@Override
