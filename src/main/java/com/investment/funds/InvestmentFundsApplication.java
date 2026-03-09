cfbe29d3d5e4f137a370f79948ee5ae53dd60ec5
Lenin Guerrero Ramírez <faulk65@gmail.com>
2026-03-09 09:01:31 -0500

    fix: extender SpringBootServletInitializer para habilitar contexto web en Lambda

--- src/main/java/com/investment/funds/InvestmentFundsApplication.java
+++ src/main/java/com/investment/funds/InvestmentFundsApplication.java
@@ -2,21 +2,24 @@ package com.investment.funds;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
 @SpringBootApplication
 @EnableMongoRepositories(basePackages = "com.investment.funds.infrastructure.adapter.mongodb.repository")
-public class InvestmentFundsApplication {
+public class InvestmentFundsApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(InvestmentFundsApplication.class, args);
 	}
-	
+
 	@org.springframework.context.annotation.Bean
 	public org.springframework.boot.CommandLineRunner logConfig(org.springframework.context.ApplicationContext ctx) {
 		return args -> {
-			System.out.println(">>> DEBUG: Active Profiles: " + java.util.Arrays.toString(ctx.getEnvironment().getActiveProfiles()));
-			System.out.println(">>> DEBUG: Logging Level DispatcherServlet: " + ctx.getEnvironment().getProperty("logging.level.org.springframework.web.servlet.DispatcherServlet"));
+			System.out.println(">>> DEBUG: Active Profiles: "
+					+ java.util.Arrays.toString(ctx.getEnvironment().getActiveProfiles()));
+			System.out.println(">>> DEBUG: Logging Level DispatcherServlet: " + ctx.getEnvironment()
+					.getProperty("logging.level.org.springframework.web.servlet.DispatcherServlet"));
 		};
 	}
 }
